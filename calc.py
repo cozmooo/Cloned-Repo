678e12e75ab0a9c30d086dd582d2fd3034dadf57
cozmooo <cozmogit@gmail.com>
2018-07-07 00:14:18 -0400

    ADDED Square function by returning x*xs

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    pass
+def subtract(x,y):
+    pass
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return  x/y
+# added square function 
+def square(x):
+    return x*x
